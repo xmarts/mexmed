8cf9f741eed3aa747b084ed949780b4957634d2a
Elymar <pablo.osorio@xmarts.com>
2020-07-15 09:12:51 -0500

    [FIX]Search by lots in product.templat

--- stock_barcode_mexmed/models/__init__.py
+++ stock_barcode_mexmed/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import product_product
-#from . import stock_scrap
-#from . import stock_picking
+from . import product_template
 from . import stock_production_lot
